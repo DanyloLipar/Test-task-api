8e657583136f07fa540936af2425c2fe569ca67b
DanyloLipar <danilo.lipar2016@gmail.com>
2022-06-23 21:05:05 +0300

    modified

--- serve.js
+++ serve.js
@@ -10,33 +10,35 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.get('/users', (req, res) => {
-    let {search, quantity, page, sort } = req.query;
+    let { search, quantity, page, sort } = req.query;
     let users = JSON.parse(fs.readFileSync('users.json'));
     const length = users.length;
-    users = users.filter(user => {
-        if(user.name.indexOf(search)!=-1) {
-            return true;
-        }
-        for (let i=0;i < user.phoneNumbers.length; i++){
-            if(user.phoneNumbers[i].indexOf(search)!=-1) {
+    if (search) {
+        users = users.filter(user => {
+            if (user.name.indexOf(search) != -1) {
                 return true;
             }
-        }
-        return false;
-    })
-    if(sort) {
-        users = users.sort((a, b) => (sort=='desc' ? sortName(a,b) : sortName(b,a)));
+            for (let i = 0; i < user.phoneNumbers.length; i++) {
+                if (user.phoneNumbers[i].indexOf(search) != -1) {
+                    return true;
+                }
+            }
+            return false;
+        })
     }
-    users = users.splice((page -1 ) * quantity, quantity);
-    res.send({users, length})
+    if (sort) {
+        users = users.sort((a, b) => (sort == 'desc' ? sortName(a, b) : sortName(b, a)));
+    }
+    users = users.splice((page - 1) * quantity, quantity);
+    res.send({ users, length })
 })
 
-app.get('/userbyid/:uid', (req, res) => {
+app.get('/getuserbyid/:uid', (req, res) => {
     let users = JSON.parse(fs.readFileSync('users.json'));
     res.send(users.find(user => user.id == req.params.uid));
 })
@@ -48,7 +50,7 @@ app.post('/adduser', (req, res) => {
         name: req.body.name,
         phoneNumbers: req.body.phoneNumbers
     })
-    fs.writeFile('users.json', JSON.stringify(users),()=>{
+    fs.writeFile('users.json', JSON.stringify(users), () => {
         res.send('User added successfully')
     });
 })
@@ -56,8 +58,22 @@ app.post('/adduser', (req, res) => {
 app.delete('/deleteuser/:uid', (req, res) => {
     let users = JSON.parse(fs.readFileSync('users.json'));
     users = users.filter(user => user.id != req.params.uid);
-    fs.writeFile('users.json', JSON.stringify(users),()=>{
+    fs.writeFile('users.json', JSON.stringify(users), () => {
         res.send('User deleted successfully')
+    });
+})
+
+app.put('/updateuser/:uid', (req, res) => {
+    let users = JSON.parse(fs.readFileSync('users.json'));
+    let { name, phoneNumbers } = req.body;
+    users = users.map(user => {
+        if (user.id != req.params.uid) {
+            return user;
+        }
+        return { ...user, name, phoneNumbers }
+    });
+    fs.writeFile('users.json', JSON.stringify(users), () => {
+        res.send('User updated successfully')
     });
 })
 
